27e382a941e9f5d9e8a1fa98c28a2354b251ceb6
Nicholas Lee <nlee@bellycard.com>
2015-02-17 20:07:23 +0000

    device object holds id and identifier_type

--- lib/device.rb
+++ lib/device.rb
@@ -1,9 +1,23 @@
 class Device
-  attr :id
+  attr :id, :identifier_type
 
   def initialize(identifier)
-    raise "Valid UDID or Serial Number required" unless is_serial?(identifier) || is_udid?(identifier)
-    id = identifier
+    if is_serial?(identifier)
+      @identifier_type = :serial
+    elsif is_udid?(identifier)
+      @identifier_type = :udid
+    else
+      raise "Valid UDID or Serial Number required"
+    end
+    @id = identifier
+  end
+
+  def device_id_param
+    if identifier_type == :serial
+      "sn-!-#{ id }"
+    else
+      id
+    end
   end
 
   private
--- spec/lib/device_spec.rb
+++ spec/lib/device_spec.rb
@@ -8,19 +8,38 @@ describe Device do
   describe 'initialization' do
     context "with serial number" do
       it "is valid" do
-        expect{ Device.new(sn) }.not_to raise_error
+        device = Device.new(sn)
+        expect(device.id).to eq sn
+        expect(device.identifier_type).to eq :serial
       end
     end
 
     context "with UDID" do
       it "is valid" do
-        expect{ Device.new(udid) }.not_to raise_error
+        device = Device.new(udid)
+        expect(device.id).to eq udid
       end
     end
 
     context "with invalid identifier" do
       it "is invalid" do
         expect{ Device.new() }.to raise_error
+      end
+    end
+  end
+
+  describe "#device_id_param" do
+    describe "Serial Number" do
+      it "prepends serial number data" do
+        device = Device.new(sn)
+        expect(device.device_id_param).to eq "sn-!-#{ sn }"
+      end
+    end
+
+    describe "UDID" do
+      it "prepends serial number data" do
+        device = Device.new(udid)
+        expect(device.device_id_param).to eq udid
       end
     end
   end
