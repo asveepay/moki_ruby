1858abb964a60f2ebef302a12c96357c87f3027d
Nicholas Lee <nlee@bellycard.com>
2015-02-17 21:29:13 +0000

    MokiRuby::Device can install app

--- spec/lib/device_spec.rb
+++ spec/lib/device_spec.rb
@@ -4,6 +4,7 @@ require 'moki_ruby/device'
 describe MokiRuby::Device do
   let(:sn) { "ABCDEFGHIJ12" }
   let(:udid) { "abcd1234-1234-1234-1234-abcdef123456" }
+  let(:device) { MokiRuby::Device.new(udid) }
 
   describe 'initialization' do
     context "with serial number" do
@@ -40,6 +41,37 @@ describe MokiRuby::Device do
       it "prepends serial number data" do
         device = MokiRuby::Device.new(udid)
         expect(device.device_id_param).to eq udid
+      end
+    end
+  end
+
+  describe "#profiles" do
+    it "calls MokiAPI.device_profile_list with correct params" do
+      expect(MokiAPI).to receive(:device_profile_list).with(udid)
+      device.profiles
+    end
+  end
+
+  describe "#install_app" do
+    describe "store app" do
+      it "calls MokiApi.perform with store app parameters" do
+
+        params = { "action" => "install_app",
+                   "thirdPartyUser" =>  "itsmebro",
+                   "clientName" =>  "Some Client Name",
+                   "itemName"  =>  "MokiTouch2.0",
+                   "notify" =>  true,
+                   "payload" =>  { "ManagementFlags" => 1,
+                                   "identifier" => "com.mokimobility.mokitouch2",
+                                   "version" => "1.1.1" } }
+
+        expect(MokiAPI).to receive(:perform_action).with(device.device_id_param, params)
+        device.install_app
+      end
+    end
+
+    describe "enterprise app" do
+      it "calls MokiApi.perform with enterprise app parameters" do
       end
     end
   end
--- spec/lib/moki_api_spec.rb
+++ spec/lib/moki_api_spec.rb
@@ -41,24 +41,13 @@ describe MokiAPI do
     end
 
     describe "device profile list" do
-      it 'hits the endpoint correctly with a UDID' do
+      it 'issues a get request using device parameter' do
+        param = "sn-!-ABCDEFGHIJ12" 
         expect(MokiAPI).to receive(:issue_request) { |method, url, options|
           expect(method).to eq(:get)
-          expect(url).to eq("http://localhost:9292/rest/v1/api/tenants/#{ ENV['MOKI_TENANT_ID'] }/devices/abcd1234-1234-1234-1234-abcdef123456/profiles")
+          expect(url).to eq("http://localhost:9292/rest/v1/api/tenants/#{ ENV['MOKI_TENANT_ID'] }/devices/#{ param }/profiles")
         }.and_return('{}')
-        MokiAPI.device_profile_list("abcd1234-1234-1234-1234-abcdef123456")
-      end
-
-      it 'hits the endpoint correctly with a serial number' do
-        expect(MokiAPI).to receive(:issue_request) { |method, url, options|
-          expect(method).to eq(:get)
-          expect(url).to eq("http://localhost:9292/rest/v1/api/tenants/#{ ENV['MOKI_TENANT_ID'] }/devices/sn-!-ABCDEFGHIJ12/profiles")
-        }.and_return('{}')
-        MokiAPI.device_profile_list("ABCDEFGHIJ12")
-      end
-
-      it 'raises an error if not given a serial or udid' do
-        expect { MokiAPI.device_profile_list("ermishness-nope") }.to raise_error
+        MokiAPI.device_profile_list(param)
       end
     end
 
@@ -118,6 +107,19 @@ describe MokiAPI do
         expect(url).to eq("http://localhost:9292/rest/v1/api/tenants/#{ ENV['MOKI_TENANT_ID'] }/iosmanagedapps")
       }.and_return('{}')
       MokiAPI.tenant_managed_app_list
+    end
+  end
+
+  describe "perform action" do
+    it 'performs put request to action endpoint with provided parameters' do
+      body_hash = { foo: 'bar' }
+      expect(MokiAPI).to receive(:issue_request) { |method, url, options|
+        expect(method).to eq(:put)
+        expect(url).to eq("http://localhost:9292/rest/v1/api/tenants/#{ ENV['MOKI_TENANT_ID'] }/devices/abcd1234-1234-1234-1234-abcdef123456/actions")
+        expect(options).to eq body_hash
+      }.and_return('{}')
+
+      MokiAPI.perform_action('abcd1234-1234-1234-1234-abcdef123456', body_hash)
     end
   end
 end
