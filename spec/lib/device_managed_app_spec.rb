66ec267e7c9f70b77921ad43858f79a788392495
Trey Springer <dfsiii@gmail.com>
2015-02-17 08:22:01 -0600

    remove unnecdssary element of spec

--- spec/lib/device_managed_app_spec.rb
+++ spec/lib/device_managed_app_spec.rb
@@ -6,8 +6,6 @@ describe DeviceManagedApp do
                           "appIdentifier" => "com.belly.gem.moki.enterprise",
                           "ManagementFlags" => 0 } }
 
-  let(:attrs) { %w(Status appIdentifier ManagementFlags) }
-
   it "will load from a hash with string keys" do
     profile = DeviceManagedApp.from_hash(response_hash)
     expect(profile.status).to eq(response_hash["Status"])
@@ -17,11 +15,10 @@ describe DeviceManagedApp do
 
   it "will convert the response to a hash" do
     profile = DeviceManagedApp.new
-    attrs.each do |attribute|
-      profile.status = "Managed"
-      profile.app_identifier = "com.belly.gem.moki.enterprise"
-      profile.management_flags = 0
-    end
+    profile.status = "Managed"
+    profile.app_identifier = "com.belly.gem.moki.enterprise"
+    profile.management_flags = 0
+
     expect(profile.to_hash).to eq(response_hash)
   end
 end
