40c49c79d0952a966eedb5257b73ad015d0061c0
Trey Springer <dfsiii@gmail.com>
2015-02-16 20:57:57 -0600

    check error raising on main object

--- lib/moki_ruby.rb
+++ lib/moki_ruby.rb
@@ -1,5 +1,25 @@
 require "moki_ruby/version"
 
 module MokiRuby
-  # Your code goes here...
+  describe ".iosprofiles" do
+    it "raises an error if no tenant ID is provided" do
+      ENV['MOKI_API_URL'] = 'http://localhost:9292'
+      ENV['MOKI_TENANT_ID'] = ''
+      ENV['MOKI_API_KEY'] = 'secret-key'
+
+      expect { MokiRuby.iosprofiles }.to raise_error
+    end
+
+    it "raises an error if no API key is provided" do
+      ENV['MOKI_API_URL'] = 'http://localhost:9292'
+      ENV['MOKI_TENANT_ID'] = 'abcd123-test'
+      ENV['MOKI_API_KEY'] = ''
+
+      expect { MokiRuby.iosprofiles }.to raise_error
+    end
+
+    xit "retuns an array of IOSProfile objects" do
+
+    end
+  end
 end
