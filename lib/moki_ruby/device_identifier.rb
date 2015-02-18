3e05e76eda1e93e5f63c9581fed27c4a1a2347f6
Trey Springer <dfsiii@gmail.com>
2015-02-18 16:28:18 -0600

    remove device identifier module, as it is baked into the device object

--- lib/moki_ruby/device_identifier.rb
+++
@@ -1,9 +0,0 @@
-module DeviceIdentifier
-  def self.is_serial?(id)
-    (!id.nil? && id.length == 12)
-  end
-
-  def self.is_udid?(id)
-    !(/\A[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\Z/.match(id)).nil?
-  end
-end
