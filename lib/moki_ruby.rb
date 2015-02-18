ee191e316004a663339d232ffa5e2bd2d284773c
Trey Springer <dfsiii@gmail.com>
2015-02-18 09:54:23 -0600

    remove MokiRuby device-specific methods, as those can be called from the device object now

--- lib/moki_ruby.rb
+++ lib/moki_ruby.rb
@@ -11,18 +11,8 @@ module MokiRuby
     data.body.map { |profile| IOSProfile.from_hash(profile) }
   end
 
-  def self.device_profiles(device_id)
-    data = MokiAPI.device_profile_list(device_id).value
-    data.body.map { |profile| IOSProfile.from_hash(profile) }
-  end
-
   def self.tenant_managed_apps
     data = MokiAPI.tenant_managed_app_list.value
     data.body.map { |profile| TenantManagedApp.from_hash(profile) }
-  end
-
-  def self.device_managed_apps(device_id)
-    data = MokiAPI.device_managed_app_list(device_id).value
-    data.body.map { |app| DeviceManagedApp.from_hash(app) }
   end
 end
