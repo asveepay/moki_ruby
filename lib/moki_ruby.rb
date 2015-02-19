167775aab57d71da086e04ca9c84ab56eec89772
Trey Springer <dfsiii@gmail.com>
2015-02-19 12:41:18 -0600

    remove action method, as it is now located on the device

--- lib/moki_ruby.rb
+++ lib/moki_ruby.rb
@@ -1,11 +1,6 @@
 require "moki_ruby/version"
 
 module MokiRuby
-  def self.action(device_id, action_id)
-    data = MokiAPI.action(device_id, action_id).value
-    Action.from_hash(data.body)
-  end
-
   def self.ios_profiles
     data = MokiAPI.ios_profiles.value
     data.body.map { |profile| IOSProfile.from_hash(profile) }
